33ca90b46cf17d2fc35e1160d6a477554fbca3e8
Severin Raevskii <severin.raevskii@omniva.ee>
2025-11-25 02:31:52 +0200

    fixing x api middleware

--- internal/middleware/x_api.go
+++ internal/middleware/x_api.go
@@ -1,27 +1,24 @@
 package middleware
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func APIKeyAuth(correctKey string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		if correctKey == "" {
-			ctx.Next()
-		}
+		if correctKey != "" {
+			apiKey := ctx.GetHeader("X-API-Key")
 
-		apiKey := ctx.GetHeader("X-API-Key")
+			if apiKey == "" {
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "X-API-Key header is missing"})
+				return
+			}
 
-		if apiKey == "" {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "X-API-Key header is missing"})
-			return
-		}
-
-		if apiKey != correctKey {
-			ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Invalid API key"})
-			return
+			if apiKey != correctKey {
+				ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Invalid API key"})
+				return
+			}
 		}
 
 		ctx.Next()
